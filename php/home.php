b59915b878ef923bed643763f0545cd4c8d9372b
Alexrupa27 <alexrupa123@gmail.com>
2025-01-21 14:59:00 +0100

    Update home.php

--- php/home.php
+++ php/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PistaChad</title>
-    <link rel="stylesheet" href="./home.css">
+    <link rel="stylesheet" href="../css/home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
@@ -27,6 +27,6 @@
     <footer>
         <p>&copy; 2025 PistaChad. Todos los derechos reservados.</p>
     </footer>
-    <script src="./home.js"></script>
+    <script src="../js/home.js"></script>
 </body>
 </html>
